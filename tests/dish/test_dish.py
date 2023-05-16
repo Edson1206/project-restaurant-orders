ad2fa2a5c87f067831b369cd34c494ad1bc63d24
Edson Pereira <ed.1206@gmail.com>
2023-05-16 17:50:36 -0300

    test: :white_check_mark: Implemented tests for `Dish` class

--- tests/dish/test_dish.py
+++ tests/dish/test_dish.py
@@ -1,7 +1,22 @@
 from src.models.dish import Dish  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient
+import pytest
 
 
 # Req 2
 def test_dish():
+    dish = Dish("batata", 450.00)
+    ingredient = Ingredient("batata")
+    dish.add_ingredient_dependency(ingredient, 450)
 
-    pass
+    with pytest.raises(TypeError):
+        Dish("name", None)
+    with pytest.raises(ValueError):
+        Dish("name", -10.00)
+
+    assert dish.name == "batata"
+    assert dish.get_restrictions() == set()
+    assert dish.__repr__() == "Dish('batata', R$450.00)"
+    assert dish == dish
+    assert hash(dish) == hash(repr(dish))
+    assert dish.get_ingredients() == set({ingredient})
