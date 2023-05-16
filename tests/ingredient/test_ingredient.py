2518384f48534125b4a7f790b0660997ae84157e
Edson Pereira <ed.1206@gmail.com>
2023-05-16 16:15:56 -0300

    test: :white_check_mark: Implemented tests for `Ingredient` class

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -3,4 +3,13 @@ from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
 
 # Req 1
 def test_ingredient():
-    pass
+    ingredient = Ingredient('gluten')
+    ingredient2 = Ingredient('lactose')
+
+    assert ingredient.name == 'gluten'
+    assert ingredient2.name == 'lactose'
+    assert hash(ingredient) == hash('gluten')
+    assert repr(ingredient) == "Ingredient('gluten')"
+    assert ingredient.restrictions == set()
+    assert ingredient == ingredient
+    assert ingredient != ingredient2
